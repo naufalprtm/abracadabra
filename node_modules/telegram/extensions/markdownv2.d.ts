c710a7285c40d129558d4b8bfd1d972b8a2c203f
naufalprtm <pratamanaufal512@gmail.com>
2024-09-27 18:22:29 +0700

    Remove node_modules and package.json from tracking

--- node_modules/telegram/extensions/markdownv2.d.ts
+++
@@ -1,5 +0,0 @@
-import { Api } from "../tl";
-export declare class MarkdownV2Parser {
-    static parse(message: string): [string, Api.TypeMessageEntity[]];
-    static unparse(text: string, entities: Api.TypeMessageEntity[] | undefined): string;
-}
